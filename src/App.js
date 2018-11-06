4d4027e95720c8b71c32b1cd7da00bc439794ac5
QuentinLeBourles <kahi.qlb@gmail.com>
2018-11-06 17:43:05 +0100

    first d3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BarChart from './test';
+
 
 class App extends Component {
   render() {
@@ -19,6 +21,8 @@ class App extends Component {
           >
             Learn React
           </a>
+          <BarChart
+            data={[5,10,1,3]} size={[500,500]}/>
         </header>
       </div>
     );
